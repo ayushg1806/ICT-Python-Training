241f75a772e552c47534795b64db86b02d38321c
Ayush Gupta <133503446+ayushg1806@users.noreply.github.com>
2025-08-08 13:24:02 +0530

    Delete myproject directory

--- myproject/todo_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.todo_list, name='todo_list'),
-    path('add/', views.add_todo, name='add_todo'),
-    path('edit/<int:id>/', views.edit, name='edit'),
-    path('delete/<int:item_id>/', views.delete_todo, name='delete'),
-]
